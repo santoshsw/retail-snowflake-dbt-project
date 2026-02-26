12e8054113087973e704429b40960dff9bb2ffd4
santoshsw <santoshs.waghmare001.com>
2026-02-26 15:57:08 +0530

    Added macro

---
+++ retail_analytics/macros/generate_schema_name.sql
@@ -0,0 +1,13 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set defualt_schema = target.schema -%}  
+    {%- if custom_schema_name is none -%}  
+
+        {{ defualt_schema }}
+    
+    {%- else -%} 
+
+        {{ custom_schema_name | trim }}
+    
+    {%- endif -%}
+
+{%- endmacro %}
